59a7cf44848590fa3ad93b381265f032ec392dad
caritoecc98 <carolina.castaneda@alumnos.ucn.cl>
2024-05-27 20:51:16 -0400

    arreglos

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilitar CORS
   app.enableCors({
-    origin: 'http://localhost:3000', // Cambia esto por el dominio de tu aplicación frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Especifica los métodos permitidos según tu API
-    allowedHeaders: ['Authorization', 'Content-Type'], // Especifica los encabezados permitidos según tu aplicación
-    credentials: true, // Habilita el intercambio de credenciales si es necesario
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    credentials: true, 
   });
 
-  // Configurar el ValidationPipe global
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // Eliminar campos no definidos en los DTOs
-      forbidNonWhitelisted: true, // Rechazar solicitudes con campos no definidos en los DTOs
-      transform: true, // Convertir automáticamente los tipos de datos de las solicitudes según los DTOs
+      whitelist: true,
+      forbidNonWhitelisted: true, 
+      transform: true, 
     }),
   );
 
